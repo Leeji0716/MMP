622f6a130b010ced067409624972e44ed125a52d
seongeon <127843337+hanseongeon@users.noreply.github.com>
2024-06-03 16:13:59 +0900

    Hse (#5)
    * securityconfig apply
    
    * pass,siteuser entitiy apply
    
    * siteuser,pass DB update
    
    * 123

--- src/main/java/com/example/MMP/siteuser/SiteUser.java
+++ src/main/java/com/example/MMP/siteuser/SiteUser.java
@@ -1,9 +1,13 @@
 package com.example.MMP.siteuser;
 
+<<<<<<< HEAD
+import jakarta.persistence.*;
+=======
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+>>>>>>> 19be8360c202ba966e58c023bca68681bcf6ea28
 import lombok.Getter;
 import lombok.Setter;
 
