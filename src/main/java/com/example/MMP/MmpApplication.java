d032a680c2bb3184910221c0b9bdef9989c23085
hanseongeon <gkstjddjs0517@gmail.com>
2024-06-03 11:28:14 +0900

    start

---
+++ src/main/java/com/example/MMP/MmpApplication.java
@@ -0,0 +1,13 @@
+package com.example.MMP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmpApplication.class, args);
+	}
+
+}
