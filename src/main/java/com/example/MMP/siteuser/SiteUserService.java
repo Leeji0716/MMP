7937587f08bbaf3cdaa3308ac2926cfa578097e0
seongeon <127843337+hanseongeon@users.noreply.github.com>
2024-06-04 17:30:47 +0900

    adminsignup modift (#24)

--- src/main/java/com/example/MMP/siteuser/SiteUserService.java
+++ src/main/java/com/example/MMP/siteuser/SiteUserService.java
@@ -9,10 +9,10 @@ import org.springframework.stereotype.Service;
 public class SiteUserService {
 private final SiteUserRepository siteUserRepository;
 private final PasswordEncoder passwordEncoder;
-private int adminCount = 1;
+
+
 public void adminSignup(String name,String number,String gender,String birthDay,String email){
     SiteUser siteUser = new SiteUser();
-    siteUser.setUserId("admin"+adminCount);
     siteUser.setPassword(passwordEncoder.encode(number));
     siteUser.setName(name);
     siteUser.setNumber(number);
@@ -21,7 +21,8 @@ public void adminSignup(String name,String number,String gender,String birthDay,
     siteUser.setEmail(email);
     siteUser.setUserRole("admin");
     siteUserRepository.save(siteUser);
-    adminCount++;
+    siteUser.setUserId("admin"+siteUser.getId());
+    siteUserRepository.save(siteUser);
 }
 
 }
