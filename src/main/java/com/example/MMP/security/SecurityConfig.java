f174e4b79712c64d0b7799f00c1eb4fce35caaea
seongeon <127843337+hanseongeon@users.noreply.github.com>
2024-06-05 16:21:36 +0900

    toss payments apply (#36)
    * 123
    
    * toss payment apply

--- src/main/java/com/example/MMP/security/SecurityConfig.java
+++ src/main/java/com/example/MMP/security/SecurityConfig.java
@@ -33,7 +33,9 @@ public class SecurityConfig{
                             new AntPathRequestMatcher("/day/**"),
                             new AntPathRequestMatcher("/pt/**"),
                             new AntPathRequestMatcher("/notice/**"), // CSRF 보호에서 제외
-                            new AntPathRequestMatcher("/user/**")
+                            new AntPathRequestMatcher("/user/**"),
+                            new AntPathRequestMatcher("/success"),
+                            new AntPathRequestMatcher("/**")
                     ))
             ;
 
--- src/main/java/com/example/MMP/siteuser/SiteUserController.java
+++ src/main/java/com/example/MMP/siteuser/SiteUserController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
