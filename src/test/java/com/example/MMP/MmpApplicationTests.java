35cffd6b06e730ec0cf4d4841f8c6e900dc6f1ee
taehun7497 <148634000+taehun7497@users.noreply.github.com>
2024-06-10 09:43:03 +0900

    commit (#55)
    Co-authored-by: taehun7497 <kth7497@gmail.com>

--- src/test/java/com/example/MMP/MmpApplicationTests.java
+++ src/test/java/com/example/MMP/MmpApplicationTests.java
@@ -1,11 +1,21 @@
-package com.example.MMP;
-
-import com.example.MMP.siteuser.SiteUser;
-import com.example.MMP.siteuser.SiteUserRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MmpApplicationTests {
-}
+//package com.example.MMP;
+//
+//import com.example.MMP.notice.NoticeService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MmpApplicationTests {
+//    @Autowired
+//    private NoticeService noticeService;
+//
+//    @Test
+//    void testJpa() {
+//        for (int i = 1; i <= 300; i++) {
+//            String title = String.format("테스트 데이터입니다");
+//            String content = "내용무";
+//            this.noticeService.create(title, content);
+//        }
+//    }
+//}
