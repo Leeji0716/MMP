8e30aa885c328710e30d5363cd6ac77e7a5d6605
DowonNam <161661393+DowonNam@users.noreply.github.com>
2024-06-20 17:29:05 +0900

    Dw (#169)
    * add the profile
    
    * add the challenge
    
    * re
    
    * morning
    
    * re
    
    * add attendance
    
    * re
    
    * re
    
    * change the main
    
    * re
    
    * publish in 240620 morning
    
    * add the group
    
    * add the challenge

--- src/main/java/com/example/MMP/MainController.java
+++ src/main/java/com/example/MMP/MainController.java
@@ -1,5 +1,8 @@
 package com.example.MMP;
 
+import com.example.MMP.challenge.attendance.Attendance;
+import com.example.MMP.challenge.attendance.AttendanceRepository;
+import com.example.MMP.challenge.attendance.AttendanceService;
 import com.example.MMP.information.Information;
 import com.example.MMP.information.InformationService;
 import lombok.RequiredArgsConstructor;
@@ -7,15 +10,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MainController {
     private final InformationService informationService;
+    private final AttendanceRepository attendanceRepository;
 
     @GetMapping("/")
-    public String mainPage(Model model){
-        Information information = informationService.getInformation();
-        model.addAttribute("information", information);
+    public String mainPage(Model model) {
+        Information information = informationService.getInformation ();
+
+        List<Attendance> attendances = attendanceRepository.findAll ();
+        int presentCount = 0;
+
+        if (!attendances.isEmpty ()) {
+            for (Attendance attendance : attendances) {
+                if (attendance.isPresent () == false) {
+                    presentCount++;
+                }
+            }
+        }
+
+        model.addAttribute ("information", information);
+        model.addAttribute ("presentCount", presentCount);
         return "schedule";
     }
 }
