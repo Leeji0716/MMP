07050cac9e7138933af9e35d60986fdb149611f1
Leeji0716 <161661370+Leeji0716@users.noreply.github.com>
2024-06-21 11:48:44 +0900

    Jy (#175)
    * 123
    
    * like
    
    * 2
    
    * user points success

--- src/main/java/com/example/MMP/coupon/CouponController.java
+++ src/main/java/com/example/MMP/coupon/CouponController.java
@@ -1,5 +1,7 @@
 package com.example.MMP.coupon;
 
+import com.example.MMP.siteuser.SiteUser;
+import com.example.MMP.siteuser.SiteUserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -16,6 +19,7 @@ import java.util.List;
 @RequestMapping("/coupon")
 public class CouponController {
     private final CouponService couponService;
+    private final SiteUserService siteUserService;
 
     @GetMapping("/create")
     public String create(CouponForm couponForm){
@@ -33,9 +37,13 @@ public class CouponController {
     }
 
     @GetMapping("/list")
-    public String list(Model model){
+    public String list(Model model, Principal principal){
         List<Coupon> couponList = couponService.getAll();
+        SiteUser siteUser = siteUserService.findByUserName(principal.getName());
+        String points = String.valueOf(siteUser.getPoint().getPoints());
+
         model.addAttribute("couponList", couponList);
+        model.addAttribute("points", points);
         return "coupon/coupon_list";
     }
 
